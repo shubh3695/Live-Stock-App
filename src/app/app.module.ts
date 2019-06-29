4fadfd0ed417566222cd477abe097130b504cecf
Shubham Saxena <shubh3695@gmail.com>
2019-06-29 16:37:54 +0530

    naive search implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -14,6 +15,7 @@ import { StockContentComponent } from './stock-content/stock-content.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     SharedModule,
     PerfectScrollbarModule
   ],
--- src/app/stock-content/stock-content.component.ts
+++ src/app/stock-content/stock-content.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { WebsocketService } from 'src/services/socket.service';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
 import { IStockContent, IStockData } from 'src/base/interfaces';
 import * as moment from 'moment';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
-import { Label } from 'ng2-charts';
-import { ChartDataSets } from 'chart.js';
 import { AppService } from 'src/services/app.service';
-
+import { timer } from 'rxjs';
 @Component({
   selector: 'app-stock-content',
   templateUrl: './stock-content.component.html',
@@ -25,41 +23,34 @@ export class StockContentComponent implements OnInit, OnDestroy {
   /* Side Chart Data Starts Here*/
   private currentStock: string;
   /*Side Chart Data Ends Here */
-
+  public searchQuery: string;
   private currentTime: number;
   constructor(private stockService: WebsocketService, private appService: AppService) { }
 
   public ngOnInit() {
     this.appService.initChart();
-    this.stockService.connect(environment.ws_url).pipe(map((res) => res))
-      .subscribe({
-        next: (responses) => {
-          if (responses && responses.length > 0) {
-            responses.forEach((response) => {
-              this.currentTime = moment.now();
-              this._resolveStockContent(response);
-            });
-          }
-          console.log(responses);
-        },
-        error: (err) => console.log(err),
-        complete: () => console.log('Connection Terminated')
-      });
+    this._createConnection();
   }
 
   public loadStockChart(key: string) {
+    if (this.currentStock === key) {
+      this.appService.initChart();
+      this.currentStock = undefined;
+    }
     this.currentStock = key;
     this.appService.renewChartData({ data: this.stockContent[key].chartData, label: key },
       this.stockContent[key].chartLabels);
   }
   public resetStockHistory() {
-    this.stockContent = {};
     this.originalSource = {};
+    this.originalSource = {};
+    this.currentStock = undefined;
     this.appService.initChart();
   }
 
-  public searchStockQuery(query: any) {
-    console.log(query.target.value);
+  public searchStockQuery(query: string) {
+    this.searchQuery = query;
+    this._filterDisplayContents();
   }
 
   public getDifference(current: number, previous: number) {
@@ -67,38 +58,45 @@ export class StockContentComponent implements OnInit, OnDestroy {
     return +_DELTA + '$ (' + Number(((+_DELTA) * 100) / previous).toFixed(2) + '%)';
   }
 
-  public getTimeDifference(time) {
+  public getTimeDifference(time: number) {
     return Number((this.currentTime - time) / 1000).toFixed(0) + ' seconds ago';
   }
+
+  public ngOnDestroy() {
+    this.stockService.close();
+  }
+
+  /* Private Functions start here */
+
   private _resolveStockContent(response: IStockData) {
     response[1] = Number(response[1]).toFixed(2);
     const _STOCK: IStockContent = {
-      prev: this.stockContent[response[0]] ? this.stockContent[response[0]].current : +response[1],
+      prev: this.originalSource[response[0]] ? this.originalSource[response[0]].current : +response[1],
       current: +response[1],
       timestamp: this.currentTime,
       chartData: this._resolveGraphData(response),
       chartLabels: this._resolveGraphLabels(response),
-      max: Math.max(this.stockContent[response[0]] && this.stockContent[response[0]].max || -Infinity, +response[1]),
-      min: Math.min(this.stockContent[response[0]] && this.stockContent[response[0]].min || Infinity, +response[1]),
+      max: Math.max(this.originalSource[response[0]] && this.originalSource[response[0]].max || -Infinity, +response[1]),
+      min: Math.min(this.originalSource[response[0]] && this.originalSource[response[0]].min || Infinity, +response[1]),
     };
-    this.stockContent[response[0]] = _STOCK;
+    this.originalSource[response[0]] = _STOCK;
     if (this.currentStock === response[0]) {
       this.appService.pushChartData({ data: response[1] }, [this._getFormattedDate()]);
     }
   }
 
   private _resolveGraphLabels(response: IStockData) {
-    if (this.stockContent[response[0]]) {
-      this.stockContent[response[0]].chartLabels.push(this._getFormattedDate());
-      return this.stockContent[response[0]].chartLabels;
+    if (this.originalSource[response[0]]) {
+      this.originalSource[response[0]].chartLabels.push(this._getFormattedDate());
+      return this.originalSource[response[0]].chartLabels;
     } else {
       return [this._getFormattedDate()];
     }
   }
   private _resolveGraphData(response: IStockData): Array<number> {
-    if (this.stockContent[response[0]]) {
-      this.stockContent[response[0]].chartData.push(+response[1]);
-      return this.stockContent[response[0]].chartData;
+    if (this.originalSource[response[0]]) {
+      this.originalSource[response[0]].chartData.push(+response[1]);
+      return this.originalSource[response[0]].chartData;
     } else {
       return [+response[1]];
     }
@@ -107,7 +105,34 @@ export class StockContentComponent implements OnInit, OnDestroy {
   private _getFormattedDate() {
     return moment(this.currentTime).format('h:mm:ss a');
   }
-  public ngOnDestroy() {
-    this.stockService.close();
+
+  private _createConnection() {
+    this.stockService.connect(environment.ws_url).pipe(map((res) => res))
+      .subscribe({
+        next: (responses) => {
+          if (responses && responses.length > 0) {
+            responses.forEach((response) => {
+              this.currentTime = moment.now();
+              this._resolveStockContent(response);
+            });
+            this._filterDisplayContents();
+          }
+        },
+        error: (err) => console.log(err),
+        complete: () => console.log('Connection Terminated')
+      });
+  }
+
+  private _filterDisplayContents() {
+    if (!this.searchQuery) {
+      this.stockContent = this.originalSource;
+      return;
+    }
+    this.stockContent = {};
+    for (const key in this.originalSource) {
+      if (key.startsWith(this.searchQuery)) {
+        this.stockContent[key] = this.originalSource[key];
+      }
+    }
   }
 }
